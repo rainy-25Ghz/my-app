f3957f21dee70ba490e025d3a6101bb5b1b6d5db
rainy25Ghz <1748420480@qq.com>
2021-03-24 21:27:20 +0800

    1

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,14 +1,134 @@
-import { useEffect, useState } from 'react';
-import styles from './carousel.module.css';
+import { useEffect, useState } from "react";
+import styles from "./carousel.module.css";
+import * as React from "react";
+import { scryptSync } from "crypto";
+
+// function handleTouchStart(ev: React.TouchEvent<HTMLInputElement>): void {
+//   const touches: React.TouchList = ev.changedTouches;
+//   console.log(touches[0]);
+// }
+// function handleTouchMove(ev: React.TouchEvent<HTMLInputElement>): void {
+//   const touches: React.TouchList = ev.changedTouches;
+//   console.log(touches[0]);
+// }
 interface propsForCarousel {
-    srcs: string[]
+  srcs: string[];
+  spacing?: number;
+  itemWidth?: number;
+  itemHeight?: number;
 }
-export default function Carousel(props: propsForCarousel) {
-    return (
-        <div className={styles.container}>
-            {props.srcs.map(item => {
-                return <img src={item}></img>
-            })}
-        </div>
-    )
+interface SlideTrackStyle {
+  width: number;
+  transition: string;
+  transform: string;
 }
+function calculateStyleForSlideTrack(
+  currentIndex: number,
+  itemWidth: number,
+  length: number
+): SlideTrackStyle {
+  // {
+  //   width: itemWidth * srcs.length,
+  //   transition: "0.5s ease-in-out",
+  //   transform: `translateX(${calculateX(currentIndex,itemWidth,srcs.length)}px)`,
+  // }
+  console.log(currentIndex);
+  if (currentIndex === length)
+    return {
+      width: itemWidth * length,
+      transition: "none",
+      transform: `translateX(${0 * itemWidth}px)`,
+    };
+  return {
+    width: itemWidth * length,
+    transition: "0.5s ease-in-out",
+    transform: `translateX(${-currentIndex * itemWidth}px)`,
+  };
+}
+
+const Carousel: React.FC<propsForCarousel> = ({
+  srcs,
+  spacing = 0,
+  itemWidth = 300,
+  itemHeight = 200,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    let id: NodeJS.Timeout = setInterval(() => {
+      setCurrentIndex((currentIndex) => (currentIndex + 1) % (srcs.length + 1));
+    }, 2000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+  return (
+    <div className={styles.container} style={{ width: itemWidth }}>
+      <div
+        className={styles.slide_track}
+        style={calculateStyleForSlideTrack(
+          currentIndex,
+          itemWidth,
+          srcs.length
+        )}
+      >
+        {srcs.map((src, index) => {
+          let style = {};
+          if (currentIndex === srcs.length && index === 0)
+            style = { transform: `translateX(${srcs.length * itemWidth}px)` };
+          return (
+            <div className={styles.item_container} style={style}>
+              <img src={src} alt="img fails to load"></img>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+// if (
+//   index === currentIndex ||
+//   index === (currentIndex + 1) % srcs.length
+// )
+//         return (
+//           <div
+//             className={`${styles.item_container}\t${styles.animated}`}
+//             // onTouchStart={handleTouchStart}
+//             // onTouchMove={handleTouchMove}
+//             style={{
+//               width: `${itemWidth}px`,
+//               transform: `translateX(${
+//                 index >= currentIndex
+//                   ? -itemWidth * (index - currentIndex)
+//                   : -itemWidth * (srcs.length + index - currentIndex)
+//               }px)`,
+//             }}
+//           >
+//             {index}
+//             <img src={item} alt="img failed" />
+//           </div>
+//         );
+//       else
+//         return (
+//           <div
+//             className={styles.item_container}
+//             // onTouchStart={handleTouchStart}
+//             // onTouchMove={handleTouchMove}
+//             style={{
+//               width: `${itemWidth}px`,
+//               transform: `translateX(${
+//                 index >= currentIndex
+//                   ? itemWidth * (index - currentIndex)
+//                   : itemWidth * (srcs.length + index - currentIndex)
+//               }px)`,
+//             }}
+//           >
+//             {index}
+//             <img src={item} alt="img failed" />
+//           </div>
+//         );
+//     })}
+//   </div>
+// );
+
+export default Carousel;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './global.css';
-import Carousel from "./components/carousel"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./global.css";
+import Carousel from "./components/carousel";
 const testSrcs = [
-  "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
-  "https://docs.microsoft.com/zh-cn/microsoft-edge/devtools-guide-chromium/media/beginners-html-edit1.msft.png",
-  "https://docs.microsoft.com/zh-cn/microsoft-edge/devtools-guide-chromium/media/bing-devtools-send-feedback.msft.png"
-]
+  "https://images.unsplash.com/photo-1615346340977-cf7f5a8f3059?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1615298681607-14ec66115339?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1615287715742-c9d52dedfb2a?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://th.bing.com/th/id/OIP.EXjyhFqk5qloQMn-1X1PFQHaDX?w=329&h=159&c=7&o=5&pid=1.7",
+];
 ReactDOM.render(
   <React.StrictMode>
-    < Carousel srcs={testSrcs} />
+    <Carousel srcs={testSrcs} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
